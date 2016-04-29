840c98b2dcc2f5adef663bea0bc96312d42c0e77
unknown <u490066@DTCEE45575D8028.ent.wfb.bank.corp>
2016-04-29 10:12:53 -0500

    initial commit

---
+++ APM - Start/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+import { ProductListComponent } from './products/product-list.component';
+
+@Component({
+	selector: 'pm-app',
+	template:`
+		<div>
+			<h2>{{pageTitle}}</h2>
+			<pm-products></pm-products>
+		</div>`,
+	directives: [ProductListComponent]
+
+})
+export class AppComponent {
+	pageTitle: string = 'Product Management'
+}
