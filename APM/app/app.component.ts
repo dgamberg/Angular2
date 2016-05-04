7067f25de09ed845a1cafa0c9afd43eb46d67e53
dgamberg <dgamberg@gmail.com>
2016-05-03 19:41:59 -0500

    finished service/Started Routing

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
+import 'rxjs/Rx';
+import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
+
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
+import { WelcomeComponent } from './home/welcome.component';
 
 @Component({
 	selector: 'pm-app',
 	template:`
-		<div>
-			<h2>{{pageTitle}}</h2>
-			<pm-products></pm-products>
-		</div>`,
-	directives: [ProductListComponent],
-	providers: [ProductService]
+		<ul>
+			<li><a [routerLink]="['Welcome']" >Home</a></li>
+			<li><a [routerLink]="['Products']">Product List</a></li>
+		</ul>`,
+	directives: [ProductListComponent, ROUTER_DIRECTIVES],
+	providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 
 })
+@RouteConfig([
+	{ path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
+	{ path: '/products', name: 'Products', component: ProductListComponent }
+])
 export class AppComponent {
 	pageTitle: string = 'Product Management'
 }
