01f189738b0ed30acda450b2e2ef725559f66181
dgamberg <dgamberg@gmail.com>
2016-05-04 20:03:37 -0500

    starting routes

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -6,21 +6,34 @@ import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from 'angular2/route
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @Component({
 	selector: 'pm-app',
 	template:`
-		<ul>
-			<li><a [routerLink]="['Welcome']" >Home</a></li>
-			<li><a [routerLink]="['Products']">Product List</a></li>
-		</ul>`,
-	directives: [ProductListComponent, ROUTER_DIRECTIVES],
+		<div>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<a href="" class="navbar-brand">{{pageTitle}}</a>
+					<ul class="nav navbar-nav">
+						<li><a [routerLink] = "['Welcome']">Home</a></li>
+						<li><a [routerLink] = "['Products']">Product List</a></li>
+					</ul>
+
+				</div>
+			</nav>
+			<router-outlet></router-outlet>
+		</div>
+
+		`,
+	directives: [ ROUTER_DIRECTIVES],
 	providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 
 })
 @RouteConfig([
 	{ path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
 	{ path: '/products', name: 'Products', component: ProductListComponent }
+	{ path: '/product/:id' , name: 'ProductDetail', component: ProductDetailComponent}
 ])
 export class AppComponent {
 	pageTitle: string = 'Product Management'
